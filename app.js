8dcccbc2602691462bd95a4075aa082fdbe803b1
TatsuKenshi <rockinbg@gmail.com>
2023-07-16 23:42:43 +0200

    general route structure

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
 
+// rest of the routes
+
+// app.get("/api/v1/tasks")        - get all tasks
+// app.post("/api/v1/tasks")       - create a new task
+// app.get("/api/v1/tasks/:id")    - get a single task
+// app.patch("/api/v1/tasks/:id")  - update a task
+// app.delete("/api/v1/tasks/:id") - delete a task
+
 const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}`));

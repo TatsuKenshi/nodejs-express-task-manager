65214c42b16cc9ae3c0473de1ac1e7e1f7dd263b
TatsuKenshi <rockinbg@gmail.com>
2023-07-18 20:44:34 +0200

    the rest of the routes, basic setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,17 @@
 const getAllTasks = (req, res) => {
-  res.send("all items from the file");
+  res.send("get all tasks");
+};
+const createTask = (req, res) => {
+  res.send("create a task");
+};
+const getTask = (req, res) => {
+  res.send("get a task");
+};
+const updateTask = (req, res) => {
+  res.send("update a task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete a task");
 };
 
-module.exports = { getAllTasks };
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };

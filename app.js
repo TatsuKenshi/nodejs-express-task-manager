eb914ab35d9b3ce3f62f0c8b6fd8985f839f22ac
TatsuKenshi <rockinbg@gmail.com>
2023-07-22 19:58:28 +0200

    getAllTasks, getTask controllers done

--- app.js
+++ app.js
@@ -14,14 +14,6 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 
-// rest of the routes
-
-// app.get("/api/v1/tasks")        - get all tasks
-// app.post("/api/v1/tasks")       - create a new task
-// app.get("/api/v1/tasks/:id")    - get a single task
-// app.patch("/api/v1/tasks/:id")  - update a task
-// app.delete("/api/v1/tasks/:id") - delete a task
-
 const port = 3000;
 
 const start = async () => {
